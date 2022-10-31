ccac26d2586e752f713eea7c726b45e055e86155
Edgar Ruiz <edgararuiz@gmail.com>
2022-10-31 14:24:08 -0500

    Adds max's scripts

---
+++ calibration/cal_isotonic.R
@@ -0,0 +1,137 @@
+#' Probability calibration using isotonic regression
+#' @param probs A vector of estimated classification probabilities.
+#' @param truth A factor vector with two levels corresponding to the known
+#' outcome values.
+#' @param ... Not currently used.
+#' @examples
+#'
+#' # Simulate data using function from modeldata package
+#' set.seed(5660)
+#' tr <- sim_classification(1000)
+#' te <- sim_classification(1000)
+#' rs <- vfold_cv(tr)
+#'
+#' # Define a random forest model
+#' rf_spec <- rand_forest() %>% set_mode("classification")
+#'
+#' # Resample the model to get realistic estimates
+#' set.seed(8401)
+#' rf_rs <- rf_spec %>% fit_resamples(class ~ ., rs, control = control_resamples(save_pred = TRUE))
+#'
+#' # Save held out predictions
+#' rf_tr_holdout <- collect_predictions(rf_rs)
+#'
+#' rf_iso <- cal_isotonic(rf_tr_holdout$.pred_class_1, rf_tr_holdout$class)
+#' rf_iso
+#'
+#' # Demonstrate the new calibration using and fitted model
+#' set.seed(9451)
+#' rf_fit <- rf_spec %>% fit(class ~ ., data = tr)
+#' rf_te_pred <- augment(rf_fit, te) %>% select(class, .pred_class_1, .pred_class_2)
+#'
+#' # Apply the calibration to the new data
+#' rf_te_calibrated <- predict(rf_iso, rf_te_pred$.pred_class_1)
+#'
+#' @export
+
+# TODO this might need to be an S3 generic with methods for tune_results objects,
+# workflow sets, and general data frames (like the caret interface)
+cal_isotonic <- function(probs, truth, ...) {
+  # TODO catch options other than times and fail
+  lvls <- levels(truth)
+  # TODO allow x to be data frame with correct column names.
+  na_val <- is.na(probs) | is.na(truth)
+  probs <- probs[!na_val]
+  truth <- truth[!na_val]
+  dat <- prepare_data_isoreg(probs, truth, ...)
+  # TODO check for null
+  mod <- isoreg(dat$x, dat$y)
+  steps <- as.stepfun(mod)
+  new_x <- environment(steps)$x
+  new_y <- environment(steps)$y
+  res <- list(x = new_x, y = new_y, levels = table(truth), n = sum(!na_val))
+  class(res) <- c("cal_isotonic", "cal_binary")
+  res
+}
+
+#' @export
+print.cal_isotonic <- function (x, ...)  {
+  cat("isotonic probability calibration\n")
+  cls <- paste0(names(x$levels), collapse = ", ")
+  cat("training data: n = ", x$n, "\nclasses: ", cls, "\n", sep = "")
+  invisible(x)
+}
+
+prepare_data_isoreg <- function(prob, truth,  sampled = FALSE) {
+  # TODO check for complete separation, warn, and return null.
+  n <- length(truth)
+  ord <- order(prob)
+  y <- ifelse(truth == levels(truth)[1], 1, 0)
+  prob <- prob[ord]
+  y <- y[ord]
+  if (sampled) {
+    ind <- sort(sample(1:n, n, replace = TRUE))
+    prob <- prob[ind]
+    y <- y[ind]
+  }
+  list(x = prob, y = y)
+}
+
+
+#' @export
+predict.cal_isotonic <- function(object, new_data, ...) {
+  # TODO check for null
+  # TODO handle missing values
+  ref_ind <- findInterval(new_data, object$x)
+  res <- tibble::tibble(one = object$y[ref_ind])
+  res$two <- 1 - res$one
+  names(res) <- paste0(".pred_", names(object$levels))
+  res
+}
+
+# ------------------------------------------------------------------------------
+
+#' @rdname cal_isotonic
+#' @param times The number of bootstrapped samples
+#' @examples
+cal_isotonic_boot <- function(probs, truth, times = 5, ...) {
+  seeds <- sample.int(10000, times)
+  mods <- purrr::map(seeds, ~ boot_iso(.x, x = probs, y = truth))
+  res <- list(models = mods)
+  class(res) <- c("cal_isotonic_boot", "cal_binary")
+  res
+
+}
+
+boot_iso <- function(seed, x, y) {
+  withr::with_seed(seed, cal_isotonic(x, y, sampled = TRUE))
+}
+
+#' @export
+print.cal_isotonic_boot <- function (x, ...)  {
+  cat("bootstrapped isotonic probability calibration\n")
+  cls <- paste0(names(x$models[[1]]$levels), collapse = ", ")
+  cat("training data: n = ", x$models[[1]]$n, "\nclasses: ", cls, "\n", sep = "")
+  cat("bootstraps:", length(x$models), "\n")
+  invisible(x)
+}
+
+#' @export
+predict.cal_isotonic_boot <- function(object, new_data, ...) {
+  cls_nm <- paste0(".pred_", names(object$models[[1]]$levels))
+  purrr::map_dfr(object$models, ~ predict(.x, new_data) %>% add_rowindex()) %>%
+    setNames(c("prob", "prob2", ".row")) %>%
+    group_by(.row) %>%
+    summarize(prob = mean(prob, na.rm = TRUE), .groups = "drop") %>%
+    mutate(prob2 = 1 - prob) %>%
+    arrange(.row) %>%
+    select(-.row) %>%
+    setNames(cls_nm)
+}
+
+
+# Open questions
+# How to handle multiclass?
+# How/where to have users interface with this? After the optimizations? How would
+#   they tune them then?
+# How to plot multiple models on the same calibration plot?
---
+++ calibration/calib_sim_example.R
@@ -0,0 +1,221 @@
+library(caret)
+library(tidymodels)
+library(discrim)
+library(doMC)
+
+# ------------------------------------------------------------------------------
+
+tidymodels_prefer()
+theme_set(theme_bw())
+options(pillar.advice = FALSE, pillar.min_title_chars = Inf)
+registerDoMC(cores = parallel::detectCores())
+bins <- seq(0, 1, length.out = 39)
+# ------------------------------------------------------------------------------
+
+set.seed(661)
+tr <- sim_classification(1000)
+te <- sim_classification(1000)
+rs <- vfold_cv(tr)
+
+show_hists <- function(x) {
+  x %>%
+    ggplot(aes(x = .pred_class_1, fill = class)) +
+    geom_histogram(
+      breaks = bins,
+      position = "identity",
+      col = "white",
+      alpha = 1 / 2
+    )
+}
+
+# ------------------------------------------------------------------------------
+# Random forest
+
+rf_spec <- rand_forest() %>% set_mode("classification")
+
+set.seed(3352)
+rf_fit <- rf_spec %>% fit(class ~ ., data = tr)
+set.seed(4234)
+rf_rs <-
+  rf_spec %>%
+  fit_resamples(class ~ ., rs, control = control_resamples(save_pred = TRUE))
+rf_tr_pred <- augment(rf_fit, tr) %>%
+  select(class, .pred_class_1, .pred_class_2) %>%
+  mutate(data = "re-prediction", model = "random forest")
+rf_tr_holdout <- collect_predictions(rf_rs) %>%
+  mutate(data = "holdout", model = "random forest")
+rf_te_pred <- augment(rf_fit, te) %>%
+  select(class, .pred_class_1, .pred_class_2)
+
+
+
+
+
+# ------------------------------------------------------------------------------
+# Quadratic discriminant analysis
+
+qda_spec <- discrim_quad()
+
+set.seed(3352)
+qda_fit <- qda_spec %>% fit(class ~ ., data = tr)
+set.seed(4234)
+qda_rs <-
+  qda_spec %>%
+  fit_resamples(class ~ ., rs, control = control_resamples(save_pred = TRUE))
+qda_tr_pred <- augment(qda_fit, tr) %>%
+  select(class, .pred_class_1, .pred_class_2) %>%
+  mutate(data = "re-prediction", model = "QDA")
+qda_tr_holdout <- collect_predictions(qda_rs) %>%
+  mutate(data = "holdout", model = "QDA")
+qda_te_pred <- augment(qda_fit, te) %>%
+  select(class, .pred_class_1, .pred_class_2)
+
+
+# ------------------------------------------------------------------------------
+# K-nearest neighbors
+
+knn_spec <-
+  nearest_neighbor(neighbors = 10, weight_func = "triangular") %>%
+  set_mode("classification")
+
+knn_wflow <-
+  norm_rec %>%
+  workflow(knn_spec)
+
+set.seed(9878)
+knn_fit <- knn_wflow %>% fit(data = tr)
+set.seed(534)
+knn_rs <-
+  knn_wflow %>%
+  fit_resamples(rs, control = control_resamples(save_pred = TRUE))
+knn_tr_pred <- augment(knn_fit, tr) %>%
+  select(class, .pred_class_1, .pred_class_2) %>%
+  mutate(data = "re-prediction", model = "10-NN")
+knn_tr_holdout <- collect_predictions(knn_rs) %>%
+  mutate(data = "holdout", model = "10-NN")
+knn_te_pred <- augment(knn_fit, te) %>%
+  select(class, .pred_class_1, .pred_class_2)
+
+# ------------------------------------------------------------------------------
+
+caret::calibration(class ~ .pred_class_1, data = knn_tr_pred) %>% plot()
+caret::calibration(class ~ .pred_class_1, data = knn_tr_holdout) %>% plot()
+
+caret::calibration(class ~ .pred_class_1, data = qda_tr_pred) %>% plot()
+caret::calibration(class ~ .pred_class_1, data = qda_tr_holdout) %>% plot()
+
+caret::calibration(class ~ .pred_class_1, data = rf_tr_pred) %>% plot()
+caret::calibration(class ~ .pred_class_1, data = rf_tr_holdout) %>% plot()
+
+
+all_training_pred <-
+  bind_rows(
+  knn_tr_pred %>% select(data, model, class, .pred_class_1),
+  knn_tr_holdout %>% select(data, model, class, .pred_class_1),
+  rf_tr_pred %>% select(data, model, class, .pred_class_1),
+  rf_tr_holdout %>% select(data, model, class, .pred_class_1),
+  qda_tr_pred %>% select(data, model, class, .pred_class_1),
+  qda_tr_holdout %>% select(data, model, class, .pred_class_1)
+)
+
+all_training_pred %>%
+  ggplot(aes(x = .pred_class_1, fill = class)) +
+  geom_histogram(
+    breaks = bins,
+    position = "identity",
+    col = "white",
+    alpha = 1 / 2
+  ) +
+  facet_grid(model ~ data) +
+  theme(legend.position = "top")
+
+# ------------------------------------------------------------------------------
+
+lp_ish <- function(x, eps = 0.0005) {
+  x <- ifelse(x < eps, eps, x)
+  x <- ifelse(x > 1 - eps, 1 - eps, x)
+  binomial()$linkfun(x)
+}
+
+all_training_pred %>%
+  mutate(lp = lp_ish(.pred_class_1)) %>%
+  ggplot(aes(x = lp, fill = class)) +
+  geom_histogram(
+    position = "identity",
+    col = "white",
+    alpha = 1 / 2
+  ) +
+  facet_grid(model ~ data) +
+  theme(legend.position = "top")
+
+all_training_pred %>%
+  mutate(lp = lp_ish(.pred_class_1)) %>%
+  ggplot(aes(x = .pred_class_1, col = class)) +
+  geom_line(stat = "density") +
+  facet_grid(model ~ data) +
+  theme(legend.position = "top")
+
+
+# ------------------------------------------------------------------------------
+
+y_table <- table(tr$class)
+platt_correction <- y_table
+platt_correction[1] <- 1 - (1 / (y_table[1] + 2))
+platt_correction[2] <-     (1 / (y_table[1] + 2))
+
+rf_tr_platt <-
+  rf_tr_holdout %>%
+  mutate(class_p = ifelse(class == "class_1", platt_correction[2], platt_correction[1]))
+
+# produces a warning
+glm(class_p ~ .pred_class_1, data = rf_tr_platt, family = "binomial")
+
+
+rf_tr_holdout %>% 
+  arrange(.pred_class_1) %>% 
+  mutate(
+    pc1 = cumsum(.pred_class_1),
+    pc1_pred = ifelse(class == "class_1", 1, 0),
+    pc1_tot = cumsum(pc1_pred)
+    ) %>% 
+  select(pc1, pc1_tot) %>% 
+  arrange(-pc1) %>% 
+  ggplot() +
+  geom_line(aes(pc1_tot, pc1)) +
+  geom_line(aes(pc1_tot, pc1_tot), color = "red", linetype = 2)
+
+rf_tr_holdout %>% 
+  arrange(.pred_class_1, desc(class)) %>% 
+  mutate(
+    pc1 = cumsum(.pred_class_1),
+    pc1_pred = ifelse(class == "class_1", 1, 0),
+    pc1_tot = cumsum(pc1_pred)
+  ) %>% 
+  select(contains("pc1"), .pred_class_1) %>% 
+  arrange(-pc1) %>% 
+  ggplot() +
+  geom_line(aes(pc1, pc1_tot)) +
+  geom_line(aes(pc1_tot, pc1_tot), color = "red", linetype = 2)
+
+
+rf_tr_holdout %>% 
+  arrange(.pred_class_2, (class)) %>% 
+  mutate(
+    pc1 = cumsum(.pred_class_2),
+    pc1_pred = ifelse(class == "class_2", 1, 0),
+    pc1_tot = cumsum(pc1_pred)
+  ) %>% 
+  select(contains("pc1"), .pred_class_2) %>% 
+  arrange(-pc1)  %>% 
+  ggplot() +
+  geom_line(aes(pc1, pc1_tot)) +
+  geom_line(aes(pc1_tot, pc1_tot), color = "red", linetype = 2)
+
+
+
+rf_tr_holdout %>% 
+  mutate(
+    
+  )
+
+caret::calibration(class ~ .pred_class_2, data = rf_tr_holdout) %>% plot()
