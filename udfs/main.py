033bedcf40c10554948e1f04eafcacdce021d27b
Edgar Ruiz <edgararuiz@gmail.com>
2024-01-25 13:34:37 -0600

    More updates

--- udfs/main.py
+++ udfs/main.py
@@ -4,7 +4,30 @@ from pyspark.sql.functions import pandas_udf
 def mean_udf(v: pd.Series) -> float:
     return v.mean()
 
-def subtract_mean(pdf: pd.DataFrame) -> pd.DataFrame:
+import pyarrow as py
+def subtract_mean(pdf: py.table) -> py.table:
     # pdf is a pandas.DataFrame
     v = pdf.mpg
     return pdf.assign(mpg=v - v.mean())
+
+from pyspark.sql.functions import pandas_udf
+
+import pyarrow as py
+def subtract_mean(pdf: py.table) -> py.table:
+    # pdf is a pandas.DataFrame
+    v = pdf.mpg
+    return pdf.assign(mpg=v - v.mean())
+  
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+
+def r_apply(pdf: float) -> float:
+  pandas2ri.activate()
+  robjects.numpy2ri.activate()
+  r_func =robjects.r("function (x) x")
+  return r_func(pdf)
+
+def filter_func(iterator):
+    for pdf in iterator:
+        yield pdf
+        
--- udfs/udfs.R
+++ udfs/udfs.R
@@ -1,4 +1,5 @@
 remotes::install_github("mlverse/pysparklyr", ref = "updates")
+
 library(reticulate)
 library(sparklyr)
 pysparklyr::spark_connect_service_start()
@@ -17,10 +18,7 @@ head(udf[[1]])
 spark_disconnect(sc)
 pysparklyr::spark_connect_service_stop()
 
-library(dbplyr)
-library(dplyr)
-library(sparklyr)
-library(reticulate)
+
 sc <- spark_connect(
   method = "databricks_connect",
   cluster_id = "1026-175310-7cpsh3g8"
@@ -36,65 +34,22 @@ head(pd_udf)
 spark_disconnect(sc)
 
 
-library(dbplyr)
-library(dplyr)
-library(sparklyr)
-library(reticulate)
-sc <- spark_connect(
-  method = "databricks_connect",
-  cluster_id = "1026-175310-7cpsh3g8"
-)
-tbl_trips <- tbl(sc, in_catalog("samples", "nyctaxi", "trips"))
-pd_trips <- tbl_trips[[1]]$session
-
-r_func <- py_func(function(x) head(x))
-
-pd_trips$mapInArrow(func = r_func, schema = pd_trips$schema)
-pd_trips$mapInPandas(func = r_func, schema = pd_trips$schema)
-
-
-sc <- spark_connect(
-  method = "databricks_connect",
-  cluster_id = "1026-175310-7cpsh3g8"
-)
-tbl_mtcars <- copy_to(sc, mtcars)
-pd_mtcars <- tbl_mtcars[[1]]$session
-pd_grouped <- pd_mtcars$groupby("am")
-orig_func <- function(x) return(head(x))
-r_func <- py_func(orig_func)
-pd_grouped$applyInPandas(r_func, pd_mtcars$schema)
-
-
 library(reticulate)
 library(sparklyr)
 Sys.setenv("PYTHON_VERSION_MISMATCH" = "/Users/edgar/.virtualenvs/r-sparklyr-pyspark-3.5/bin/python")
 Sys.setenv("PYSPARK_DRIVER_PYTHON" = "/Users/edgar/.virtualenvs/r-sparklyr-pyspark-3.5/bin/python")
 pysparklyr::spark_connect_service_start()
 sc <- spark_connect("sc://localhost", method = "spark_connect", version = "3.5")
-
-library(reticulate)
-#py_install("rpy2")
-rpy2 <- import("rpy2")
-rpy2$robjects$pandas2ri$activate()
-r_source <- rpy2$robjects$r("source")
-r_sourced <- r_source("/Users/edgar/r_projects/practice/udfs/addthree.R")
-
-rpy2$robjects$pandas2ri$rinterface("addthree")
-addthree <- rpy2$robjects$r("addthree")
-
-pyspark <- import("pyspark")
-sel_col <- pyspark$sql$functions$col("mpg")
-np <- import("numpy")
 tbl_mtcars <- copy_to(sc, mtcars)
 pd_mtcars <- tbl_mtcars[[1]]$session
 pd_grouped <- pd_mtcars$groupby("am")
-
-new_mean <- function(x) np$mean(x$mpg)
-new_mean <- py_func(function(x) np$mean(x$mpg))
-
+source_python("/Users/edgar/r_projects/practice/udfs/main.py")
 main <- reticulate::import_main()
+pd_grouped$applyInPandas(main$r_apply, schema = pd_mtcars$schema)$show()
+
+
+pd_mtcars$mapInArrow(main$filter_func, schema = pd_mtcars$schema)$show()
+
+pd_grouped$applyInPandas(main$mean_udf, schema = pd_mtcars$schema)$show()
 
 pd_grouped$applyInPandas(main$subtract_mean, schema = pd_mtcars$schema)$show()
-pysparklyr::spark_connect_service_stop()
-spark_disconnect(sc)
-np$mean(1:3)
