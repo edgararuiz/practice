df77493d175080b383f3ba98baa29b315c2b4525
Edgar Ruiz <edgararuiz@gmail.com>
2024-01-25 17:06:27 -0600

    Gets lambda function to work

--- udfs/udfs.R
+++ udfs/udfs.R
@@ -44,14 +44,18 @@ sc <- spark_connect("sc://localhost", method = "spark_connect", version = "3.5")
 tbl_mtcars <- copy_to(sc, mtcars)
 pd_mtcars <- tbl_mtcars[[1]]$session
 pd_grouped <- pd_mtcars$groupby("am")
-fn <- rlang::expr_text(purrr::as_mapper(~ mean(.x$mpg) - 2)) 
+fn <- purrr::as_mapper(~ mean(.x$mpg) - 2)
+fn <- rlang::expr_text(fn) 
+fn <- paste0(unlist(strsplit(fn, "\n")), collapse = "")
+wr <- paste0("function(...){x <- ", fn,"; x(...)}")
+wr <- gsub("\"", "'", wr)
 py_run_string(
 paste0("import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri
 def r_apply(key, pdf: pd.DataFrame) -> pd.DataFrame:
   pandas2ri.activate()
-  r_func =robjects.r(\"", fn,"\")
+  r_func =robjects.r(\"", wr, "\")
   ret = r_func(pdf)
   return pd.DataFrame(ret)
 "))
