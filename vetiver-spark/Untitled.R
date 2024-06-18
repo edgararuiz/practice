7be4de303d429ce26d600dabefc9272995b08fe3
Edgar Ruiz <edgararuiz@gmail.com>
2024-06-18 15:49:59 -0500

    creates function

--- vetiver-spark/Untitled.R
+++ vetiver-spark/Untitled.R
@@ -23,3 +23,25 @@ lendingclub_local$all_util <- as.numeric(lendingclub_local$all_util)
 lendingclub_local$bc_open_to_buy <- as.numeric(lendingclub_local$bc_open_to_buy)
 
 predict(model, lendingclub_local)
+
+
+predict_vetiver <- function(x) {
+  board <- pins::board_connect(auth = "manual", server = "https://pub.demo.posit.team/", key = "brlZsHOg6DwIH9K2UDDW3msAn6TwFDF7")
+  model <- vetiver::vetiver_pin_read(board, "garrett@posit.co/lending_club_model")
+  term <- lapply(strsplit(lendingclub_local$term, " "), function(x) x[[2]])
+  x$term <- as.numeric(term)
+  x$bc_util <- as.numeric(lendingclub_local$bc_util)
+  x$all_util <- as.numeric(lendingclub_local$all_util)
+  x$bc_open_to_buy <- as.numeric(lendingclub_local$bc_open_to_buy)
+  library(workflows)
+  head(predict(model, x), 1)
+}
+
+predict_vetiver(lendingclub_local)
+
+lendingclub_dat |> 
+  spark_apply(predict_vetiver)
+
+
+lendingclub_dat |> 
+  spark_apply(nrow)
