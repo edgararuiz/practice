a562335161125e63bbba636610a48f6f53a4d42d
Edgar Ruiz <edgararuiz@gmail.com>
2024-06-19 07:56:46 -0500

    Cleaned up script

--- vetiver-spark/Untitled.R
+++ vetiver-spark/Untitled.R
@@ -1,32 +1,36 @@
 
+# One time in your machine
+pak::pak("mlverse/pysparklyr")
+pak::pak("rstudio/reticulate")
+
+# Actual script
 library(sparklyr)
 library(dplyr)
-
 sc <- spark_connect(method = "databricks_connect")
 lendingclub_dat <- tbl(sc, dbplyr::in_catalog("hive_metastore", "default", "lendingclub"))
-
-
-lendingclub_dat |> 
-  head(100) |> 
-  spark_apply(function(e) {x<-nrow(e);x+10})
-
 predict_vetiver <- function(x) {
-  board <- pins::board_connect(auth = "manual", server = "https://pub.demo.posit.team/", key = "brlZsHOg6DwIH9K2UDDW3msAn6TwFDF7")
+  board <- pins::board_connect(
+    auth = "manual", 
+    server = "https://pub.demo.posit.team/",
+    key = "brlZsHOg6DwIH9K2UDDW3msAn6TwFDF7"
+    )
   model <- vetiver::vetiver_pin_read(board, "garrett@posit.co/lending_club_model")
   term <- lapply(strsplit(x$term, " "), function(x) x[[2]])
   x$term <- as.numeric(term)
   x$bc_util <- as.numeric(x$bc_util)
   x$all_util <- as.numeric(x$all_util)
   x$bc_open_to_buy <- as.numeric(x$bc_open_to_buy)
+  x <- x[!is.na(x$bc_util), ]
+  x <- x[!is.na(x$all_util), ]
+  x <- x[!is.na(x$bc_open_to_buy), ]
   library(workflows)
   preds <- predict(model, x)
-  data.frame(mean_preds = mean(preds[, 1][[1]])))
+  data.frame(mean_preds = mean(preds[, 1][[1]]))
 }
 
 lendingclub_dat |> 
-  head(100) |> 
-  spark_apply(predict_vetiver)
-
+  head(30000) |> 
+  spark_apply(predict_vetiver, columns = "mean_preds double") 
 
 
 
