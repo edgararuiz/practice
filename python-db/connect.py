a0b5a780e7b5b1c431916c88e18bf0b59fc2114e
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-06 12:21:39 -0500

    Adds converting to table step

--- python-db/connect.py
+++ python-db/connect.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy import text
 from sqlalchemy.orm import DeclarativeBase, Session
+import pandas as pd
+
 load_dotenv()
 
-
-pip install databricks-sql-connector
+# pip install databricks-sql-connector
 
 access_token    = os.getenv("DATABRICKS_TOKEN")
 host            = os.getenv("DATABRICKS_HOST")
@@ -18,7 +19,7 @@ engine = create_engine(
     f"databricks://token:{access_token}@{host}?http_path={http_path}&catalog={catalog}&schema={schema}",
     echo=True,
 )
-
-with engine.connect() as con:
-    rs = con.execute(text('SELECT * FROM lendingclub limit 10'))
-
+session = Session(engine)
+res = session.execute(text('SELECT * FROM lendingclub limit 10'))
+df = pd.read_sql_query('SELECT * FROM lendingclub limit 10', engine)
+print(df)
