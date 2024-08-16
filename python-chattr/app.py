de87af8d88a97f4bf111d8608d0889f0983382b0
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-15 20:50:07 -0500

    Persists response

--- python-chattr/app.py
+++ python-chattr/app.py
@@ -1,17 +1,22 @@
 import random
 import subprocess
 
-proc = subprocess.Popen(['python','python-chattr/chattr.py', "--prompt='hello'"],stdout=subprocess.PIPE)
+proc = subprocess.Popen(['python','python-chattr/chattr.py', "--prompt='what is the tallest mountain?'"],stdout=subprocess.PIPE)
 
 from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 
 app_ui = ui.page_fluid(ui.output_text("value"))
 
 def server(input: Inputs, output: Outputs, session: Session):
+    response = ''
     @render.text
     def value():
-        reactive.invalidate_later(0.5)
-        return line = proc.stdout.read()
+        nonlocal response
+        reactive.invalidate_later(2)
+        out = proc.stdout.read()
+        if out:
+            response = response + str(out)
+        return response
 
 
 app = App(app_ui, server)
