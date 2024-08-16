faf1ab10fd3605cc2d8a4fdd364e08cd1721cce9
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-16 15:00:29 -0500

    starts history insertion

--- python-chattr/app.py
+++ python-chattr/app.py
@@ -5,7 +5,8 @@ from shiny import App, Inputs, Outputs, Session, reactive, render, ui
 app_ui = ui.page_fluid(
     ui.input_text("prompt", "Prompt"),
     ui.input_task_button("submit", "Submit"),
-    ui.output_text("value")
+    ui.output_text("value"),
+    ui.card(id = "main")
     )
 
 def server(input: Inputs, output: Outputs, session: Session):
@@ -30,13 +31,27 @@ def server(input: Inputs, output: Outputs, session: Session):
         nonlocal response
         nonlocal proc
         out = ''
-        reactive.invalidate_later(0.1)
+        reactive.invalidate_later(0.5)
         if hasattr(proc, "stdout"):
             out = proc.stdout.read(3)
             if out:
-                ui.update_text("prompt", value= "")
+                if input.prompt() != '':
+                    pr = "Me: " + input.prompt()
+                    ui.update_text("prompt", value= "")
+                    ui.insert_ui(                        
+                        ui.p(pr), selector= "#main", where = "afterEnd"
+                        )                        
+                    pr = ''                
                 out = str(out.decode())
+                print(out)
                 response = response + out
+            else:
+                if response != '':
+                    response = "LLLM: " + response
+                    ui.insert_ui(                        
+                        ui.p(response), selector= "#main", where = "afterEnd"
+                    )    
+                    response = ''                
         return response
 
 
