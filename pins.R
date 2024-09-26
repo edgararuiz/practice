7d5d0e369486e7de9ca166ec29da22de2ff8852c
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-26 13:45:06 -0500

    No luck

--- pins.R
+++ pins.R
@@ -8,14 +8,23 @@ host <- Sys.getenv("DATABRICKS_HOST")
 host_url <- url_parse(host)
 if (is.null(host_url$scheme)) host_url$scheme <- "https"
 
+library(purrr)
+
 paste0(
   "https://rstudio-partner-posit-default.cloud.databricks.com",
-  "/api/2.1/clusters/get"
+  "/api/2.1/unity-catalog/catalogs"
 ) |> 
   request() |> 
   req_auth_bearer_token(Sys.getenv("DATABRICKS_TOKEN")) |> 
-  req_body_json(list(cluster_id = "0916-215603-ofitqny9")) |> 
-  req_perform()
+  #req_headers(name = "workshops") |> 
+  req_headers(catalog_name = "workshops") |> 
+  req_method("GET") |> 
+  #req_dry_run()
+  req_perform() |> 
+  resp_body_json() |> 
+  list_flatten() |> 
+  map(~.x$name)
+
 
 
 paste0(
@@ -24,7 +33,7 @@ paste0(
 ) |> 
   request() |> 
   #req_body_json(list(catalog_name = "workshops")) |> 
-  req_body_json(list(catalog_name = "workshops")) |> 
+  req_headers(catalog_name = "workshops") |> 
   req_auth_bearer_token(Sys.getenv("DATABRICKS_TOKEN")) |> 
   req_perform()
 
