14bc85c6c9cbdc20d5b7ef4f09d5ad85066c3387
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-26 12:43:22 -0500

    More tries

--- pins.R
+++ pins.R
@@ -8,18 +8,35 @@ host <- Sys.getenv("DATABRICKS_HOST")
 host_url <- url_parse(host)
 if (is.null(host_url$scheme)) host_url$scheme <- "https"
 
-resp <- host_url |> 
-  url_build() |> 
+paste0(
+  "https://rstudio-partner-posit-default.cloud.databricks.com",
+  "/api/2.1/clusters/get"
+) |> 
   request() |> 
-  req_url_path_append("/api/2.1/unity-catalog/volumes") |> 
   req_auth_bearer_token(Sys.getenv("DATABRICKS_TOKEN")) |> 
-  req_body_json(
-    list(
-      catalog_name = "workshop", 
-      schema_name = "models"
-    )
-  ) |> 
+  req_body_json(list(cluster_id = "0916-215603-ofitqny9")) |> 
   req_perform()
 
-  
+
+paste0(
+  "https://rstudio-partner-posit-default.cloud.databricks.com",
+  "/api/2.0/unity-catalog/schemas"
+) |> 
+  request() |> 
+  #req_body_json(list(catalog_name = "workshops")) |> 
+  req_body_json(list(catalog_name = "workshops")) |> 
+  req_auth_bearer_token(Sys.getenv("DATABRICKS_TOKEN")) |> 
+  req_perform()
+
+
+
+request("https://rstudio-partner-posit-default.cloud.databricks.com") |> 
+  req_url_path_append("/api/2.1/unity-catalog/volumes") |> 
+  req_auth_bearer_token(Sys.getenv("DATABRICKS_TOKEN")) |> 
+  req_headers(name = "workshops.models.vetiver") |> 
+  req_perform()
+
+
+#curl --request GET "https://${DATABRICKS_HOST}api/2.1/unity-catalog/volumes/" --header "Authorization: Bearer ${DATABRICKS_TOKEN}" | jq .
+
 
