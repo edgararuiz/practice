c64d15531a514dba03566221d46da0f1afeb7298
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-30 16:46:25 -0500

    Adds prompt

--- python-chattr/mall.py
+++ python-chattr/mall.py
@@ -5,11 +5,14 @@ class MallFrame:
     def __init__(self, df: pl.DataFrame) -> None:
         self._df = df
 
-    def sentiment(self, col) -> list[pl.DataFrame]:
+    def sentiment(self, col, pred_name = "sentiment") -> list[pl.DataFrame]:
+        prompt = "You are a helpful sentiment engine. Return only one of the following" + \
+            " answers: positive, negative, neutral. No capitalization. No explanations. " +\
+            "The answer is based on the following text:\n"
         df = self._df.with_columns(  
            pl.col(col)
-            .map_elements(lambda x: len(x), return_dtype=pl.Int64)
-            .alias("a_times_2")
+            .map_elements(lambda x: prompt + x, return_dtype=pl.String)
+            .alias(pred_name)
             )
         return(df)
 
