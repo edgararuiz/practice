2a035ddc0e05c5c35c4af0570e5ef8d55c2b7728
Edgar Ruiz <edgararuiz@gmail.com>
2024-09-30 16:57:17 -0500

    Adds ollama interface, works!

--- python-chattr/mall.py
+++ python-chattr/mall.py
@@ -1,22 +1,27 @@
 import polars as pl
+import ollama
 
 @pl.api.register_dataframe_namespace("llm")
 class MallFrame:
     def __init__(self, df: pl.DataFrame) -> None:
         self._df = df
 
-    def sentiment(self, col, pred_name = "sentiment") -> list[pl.DataFrame]:
+    def sentiment(self, col, pred_name = "sentiment") -> list[pl.DataFrame]:        
         prompt = "You are a helpful sentiment engine. Return only one of the following" + \
             " answers: positive, negative, neutral. No capitalization. No explanations. " +\
             "The answer is based on the following text:\n"
         df = self._df.with_columns(  
            pl.col(col)
-            .map_elements(lambda x: prompt + x, return_dtype=pl.String)
+            .map_elements(
+                lambda x: 
+                ollama.chat(
+                    model='llama3.2', messages=[{'role': 'user','content': prompt + x,}]
+                    )['message']['content'], 
+                return_dtype=pl.String)
             .alias(pred_name)
             )
         return(df)
-
-
+        
 df = pl.DataFrame(
     data=["I am happy", "I am sad"],
     schema=[("txt", pl.String)],
