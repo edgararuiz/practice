aa388b18c576ffadad78c19ef65add1d86a8b3ba
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-08 14:43:49 -0600

    Adds Rd extraction and rendering to RStudio

--- help/help.R
+++ help/help.R
@@ -1,15 +1,19 @@
-library(ggplot2)
+library(mall)
 library(tools)
 library(fs)
-topic <- "aes"
-pkg_name <- NULL
+topic <- "llm_classify"
+package <- NULL
 help_file <- help(topic, help_type = "text")
 help_path <- as.character(help_file)
 
-if(topic == path_file(help_path) && is.null(pkg_name)) {
+if(topic == path_file(help_path) && is.null(package)) {
   help_folder <- path_dir(help_path)
   help_pkg <- path_dir(help_folder)  
-  pkg_name <- path_file(help_pkg)
+  package <- path_file(help_pkg)
 }
-db <- Rd_db(pkg_name)
-db[[path(topic, ext = "Rd")]]
+db <- Rd_db(package)
+rd_content <- db[[path(topic, ext = "Rd")]]
+rd_text <- paste0(as.character(rd_content), collapse = "")
+writeLines(rd_text, "content.Rd")
+rstudioapi::previewRd("content.Rd")
+
