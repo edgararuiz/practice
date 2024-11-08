ffa4fb77f2c2b2edd5950a37d14eefad230a9b36
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-08 17:18:51 -0600

    Starts batch translation

--- help/help.R
+++ help/help.R
@@ -4,7 +4,7 @@ library(tools)
 library(fs)
 library(pkgsite)
 
-topic <- "aes"
+topic <- "llm_classify"
 package <- NULL
 help_file <- help(topic, help_type = "text")
 help_path <- as.character(help_file)
@@ -17,17 +17,6 @@ if(topic == path_file(help_path) && is.null(package)) {
 db <- Rd_db(package)
 rd_content <- db[[path(topic, ext = "Rd")]]
 
-for(i in seq_along(rd_content)) {
-  rd_i <- rd_content[[i]]
-  print(attr(rd_i, "Rd_tag"))
-}
-
-#llm_vec_translate(paste0(rd_content[[5]], collapse = ""), "spanish")
-
-new_content <- rd_content[[1]][[1]]
-new_content[[1]] <- llm_vec_translate(new_content[[1]], "spanish")
-rd_content[[1]][[1]] <- new_content
-
 extract_text <- function(x) {
   attributes(x) <- NULL
   class(x) <- "Rd"
@@ -37,5 +26,27 @@ extract_text <- function(x) {
   rd_txt <- capture.output(Rd2txt(temp_rd, fragment = TRUE))
   paste0(rd_txt, collapse = "")
 }
-extract_text(rd_content[[8]])
 
+tag_text <- NULL
+for(i in seq_along(rd_content)) {
+  rd_i <- rd_content[[i]]
+  tag_name <- attr(rd_i, "Rd_tag")
+  if(tag_name %in% c("\\title", "\\description", "\\value", "\\details")) {
+    tag_text <- llm_vec_translate(extract_text(rd_i), "spanish")
+    attrs <- attributes(rd_i[[1]])
+    attr(attrs, "Rd_tag") <- "TEXT"
+    attributes(tag_text) <- attrs
+    obj <- list(tag_text)
+    attributes(obj) <- attributes(rd_i)
+    rd_content[[i]] <- obj
+  }
+}
+
+rd_text <- paste0(as.character(rd_content), collapse = "")
+writeLines(rd_text, "content.Rd")
+rstudioapi::previewRd("content.Rd")
+
+#llm_vec_translate(paste0(rd_content[[5]], collapse = ""), "spanish")
+# new_content <- rd_content[[1]][[1]]
+# new_content[[1]] <- llm_vec_translate(new_content[[1]], "spanish")
+# rd_content[[1]][[1]] <- new_content
