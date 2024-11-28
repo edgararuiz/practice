ebbdd512bea172e1dc7a794515db0a0c102d7650
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-28 08:54:41 -0600

    Initial map pass

--- pkgsite/pkgsitenew.R
+++ pkgsite/pkgsitenew.R
@@ -10,7 +10,9 @@ rd_extract_text <- function(x, collapse = TRUE) {
   }
   temp_rd <- tempfile(fileext = ".Rd")
   writeLines(rd_text, temp_rd)
-  rd_txt <- capture.output(tools::Rd2txt(temp_rd, fragment = TRUE))
+  suppressWarnings(
+    rd_txt <- capture.output(tools::Rd2txt(temp_rd, fragment = TRUE))  
+  )
   if (collapse) {
     rd_txt[rd_txt == ""] <- "\n\n"
     rd_txt <- paste0(rd_txt, collapse = "")
@@ -18,78 +20,92 @@ rd_extract_text <- function(x, collapse = TRUE) {
   rd_txt
 }
 
+library(purrr)
+library(rlang)
 out <- list()
-rd_content <- tools::parse_Rd("man/rd_to_qmd.Rd")
-tag_text <- NULL
-tag_name <- NULL
-tag_label <- NULL
-cli_progress_message("Translating: {.emph {tag_label}}")
-for (i in seq_along(rd_content)) {
-  rd_i <- rd_content[[i]]
-  tag_name <- attr(rd_i, "Rd_tag")
-  standard_tags <- c(
-    "\\title", "\\description",
-    "\\value", "\\details",
-    "\\seealso", "\\section"
-  )
-  if (tag_name == "\\section") {
-    tag_label <- paste0("Section: '", as.character(rd_i[[1]]), "'")
-  }
-  if (is.null(tag_label)) {
-    tag_label <- tag_label #tag_to_label(tag_name)
-  }
-  #tag_label <-  to_title(tag_label)
-  cli_progress_update()
-  if (tag_name %in% standard_tags) {
-    out[tag_name] <- rd_extract_text(rd_i)
-  }
-  if (tag_name == "\\section") {
-    out[tag_name] <- list(rd_extract_text(rd_i[[1]]), rd_extract_text(rd_i[[2]]))
-  }
-  if (tag_name == "\\arguments") {
-    args_list <- NULL
-    args <- rd_i
-    for (k in seq_along(rd_i)) {
-      rd_k <- rd_i[[k]]
-      if (length(rd_k) > 1) {
-        curr_arg <- list(argument = as.character(rd_k[[1]]), description =  rd_extract_text(rd_k[[2]]))
-        args_list <- c(args_list, curr_arg)
-      }
-    }
-    out["arguments"] <- as.list(args_list)
-  }
-  if (tag_name == "\\name") {
-    topic_name <- rd_i
-  }
-  if (tag_name == "\\examples") {
-    for (k in seq_along(rd_i)) {
-      rd_k <- rd_i[[k]]
-      rd_char <- as.character(rd_k)
-      if (length(rd_char) == 1) {
-        if (substr(rd_char, 1, 2) == "# ") {
-          last_char <- substr(rd_char, nchar(rd_char), nchar(rd_char))
-          n_char <- ifelse(last_char == "\n", 1, 0)
-          rd_char <- substr(rd_char, 3, nchar(rd_char) - n_char)
-          rd_char <- llm_vec_translate(rd_char, lang)
-          rd_char <- paste0("# ", rd_char, "\n")
-          attributes(rd_char) <- attributes(rd_k)
-          rd_i[[k]] <- rd_char
-        }
-      }
-    }
-    rd_content[[i]] <- rd_i
-  }
-}
-tag_name <- NULL
-cli_progress_update()
-rd_text <- paste0(as.character(rd_content), collapse = "")
-#rd_text
-out
-#topic_path <- fs::path(tempdir(), topic_name, ext = "Rd")
-#writeLines(rd_text, topic_path)
-#topic_path
+rd_content <- tools::parse_Rd("~/Projects/pkgsite/man/rd_to_qmd.Rd")
 
+map(rd_content, \(x) {
+  tag_name <- attr(x, "Rd_tag")
+  if(grepl("\\\\", tag_name)) {
+    tag_name <- substr(tag_name, 2, nchar(tag_name))
+    tag_text <- list(rd_extract_text(x))
+    set_names(tag_text, tag_name)
+  }
+})
 
-
-
-#rd_to_list("rd_to_qmd.Rd")
+# 
+# 
+# tag_text <- NULL
+# tag_name <- NULL
+# tag_label <- NULL
+# cli_progress_message("Translating: {.emph {tag_label}}")
+# for (i in seq_along(rd_content)) {
+#   rd_i <- rd_content[[i]]
+#   tag_name <- attr(rd_i, "Rd_tag")
+#   standard_tags <- c(
+#     "\\title", "\\description",
+#     "\\value", "\\details",
+#     "\\seealso", "\\section"
+#   )
+#   if (tag_name == "\\section") {
+#     tag_label <- paste0("Section: '", as.character(rd_i[[1]]), "'")
+#   }
+#   if (is.null(tag_label)) {
+#     tag_label <- tag_label #tag_to_label(tag_name)
+#   }
+#   #tag_label <-  to_title(tag_label)
+#   cli_progress_update()
+#   if (tag_name %in% standard_tags) {
+#     out[tag_name] <- rd_extract_text(rd_i)
+#   }
+#   if (tag_name == "\\section") {
+#     out[tag_name] <- list(rd_extract_text(rd_i[[1]]), rd_extract_text(rd_i[[2]]))
+#   }
+#   if (tag_name == "\\arguments") {
+#     args_list <- NULL
+#     args <- rd_i
+#     for (k in seq_along(rd_i)) {
+#       rd_k <- rd_i[[k]]
+#       if (length(rd_k) > 1) {
+#         curr_arg <- list(argument = as.character(rd_k[[1]]), description =  rd_extract_text(rd_k[[2]]))
+#         args_list <- c(args_list, curr_arg)
+#       }
+#     }
+#     out["arguments"] <- as.list(args_list)
+#   }
+#   if (tag_name == "\\name") {
+#     topic_name <- rd_i
+#   }
+#   if (tag_name == "\\examples") {
+#     for (k in seq_along(rd_i)) {
+#       rd_k <- rd_i[[k]]
+#       rd_char <- as.character(rd_k)
+#       if (length(rd_char) == 1) {
+#         if (substr(rd_char, 1, 2) == "# ") {
+#           last_char <- substr(rd_char, nchar(rd_char), nchar(rd_char))
+#           n_char <- ifelse(last_char == "\n", 1, 0)
+#           rd_char <- substr(rd_char, 3, nchar(rd_char) - n_char)
+#           rd_char <- llm_vec_translate(rd_char, lang)
+#           rd_char <- paste0("# ", rd_char, "\n")
+#           attributes(rd_char) <- attributes(rd_k)
+#           rd_i[[k]] <- rd_char
+#         }
+#       }
+#     }
+#     rd_content[[i]] <- rd_i
+#   }
+# }
+# tag_name <- NULL
+# cli_progress_update()
+# rd_text <- paste0(as.character(rd_content), collapse = "")
+# #rd_text
+# out
+# #topic_path <- fs::path(tempdir(), topic_name, ext = "Rd")
+# #writeLines(rd_text, topic_path)
+# #topic_path
+# 
+# 
+# 
+# 
+# #rd_to_list("rd_to_qmd.Rd")
