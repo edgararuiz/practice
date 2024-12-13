00b541de74f8671fdbd7b4233195b9abb8769d36
Edgar Ruiz <edgararuiz@gmail.com>
2024-12-13 12:18:46 -0600

    switches to processx

--- uv/uv.R
+++ uv/uv.R
@@ -3,7 +3,7 @@ library(reticulate)
 uv <- function(...) {
   system2(
     fs::path_rel("~/.local/bin/uv"),
-    paste(c(...), collapse = " "), 
+    paste(c(...), collapse = " "),
     stderr = TRUE,
     stdout = TRUE
     )
@@ -26,6 +26,47 @@ uv_install("-n", "jax")
 
 #fs::dir_delete(here::here(".venv"))
 
+# ------------------- test ------------------------------
+
 library(processx)
 
-run("uv", "venv")
+x <- run(fs::path_rel("~/.local/bin/uv"), c("-h"))
+cat(x$stdout)
+
+
+x <- process$new(fs::path_rel("~/.local/bin/uv"), c("venv"),stdout = "|", stderr = "|")
+while(x$is_alive()) {
+  cat(x$read_error())
+  cat(x$read_output())
+}
+
+# ------------------------- new -------------------------
+
+library(processx)
+library(reticulate)
+
+uv_exe <- function() {
+  fs::path_rel("~/.local/bin/uv")
+}
+
+uv_process <- function(...) {
+  x <- process$new(uv_exe(), c(...),stdout = "|", stderr = "|")
+  while(x$is_alive()) {
+    cat(x$read_error())
+    cat(x$read_output())
+  }
+  invisible()
+}
+
+uv_process("venv")
+use_virtualenv(here::here(".venv"))
+uv_install <- function(..., python = py_exe()) {
+  uv_process(
+    "pip", 
+    "install",
+    "--python", shQuote(fs::path_rel(python)),
+    c(...)
+  )
+}
+
+uv_install("polars", "jax")
