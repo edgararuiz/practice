5ff4165176f70184c9f8d59edea38b601d97e13a
Edgar Ruiz <edgararuiz@gmail.com>
2024-12-13 10:28:43 -0600

    Starts trying out installations

--- uv/uv.R
+++ uv/uv.R
@@ -1,38 +1,25 @@
+library(reticulate)
+
 uv <- function(...) {
-  reticulate:::system2t(fs::path_real("~/.local/bin/uv"), paste(c(...), collapse = " "))
+  reticulate:::system2t(fs::path_rel("~/.local/bin/uv"), paste(c(...), collapse = " "))
 }
 
 uv("venv")
-reticulate:::py_activate_virtualenv(fs::path_real(".venv/bin/"))
-
+use_virtualenv(here::here(".venv"))
 
 uv_install <- function(..., python = py_exe()) {
   uv("pip install",
-     "--python", shQuote(python),
+     "--python", shQuote(fs::path_rel(python)),
      c(...))
 }
 
-uv_venv_create <- function(envname = NULL, python_version = "3.11",
-                           packages = "numpy", ..., force = FALSE) {
-  path <- path.expand(reticulate:::virtualenv_path(envname))
-  if (force)
-    unlink(path, recursive = TRUE, force = TRUE)
-  dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  
-  uv(
-    "venv",
-    "--no-project",
-    "--seed",
-    "--no-config",
-    "--python-preference only-managed",
-    "--python", python_version,
-    ...,
-    shQuote(path)
-  )
-  
-  if (length(packages))
-    uv_install(packages, python = reticulate:::virtualenv_python(path))
-  
-  invisible(path)
-}
+uv_install("polars")
 
+uv_install("--no-progress", "pandas")
+
+uv_install("--color", "never", "jax")
+
+uv_install("--color", "never", "polars")
+
+
+#fs::dir_delete(here::here(".venv"))
