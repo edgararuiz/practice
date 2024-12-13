35870d401b64ef9572e665dc7aa1cfc22a494358
Edgar Ruiz <edgararuiz@gmail.com>
2024-12-13 10:55:30 -0600

    moves no color to main function

--- uv/uv.R
+++ uv/uv.R
@@ -1,25 +1,25 @@
 library(reticulate)
 
 uv <- function(...) {
-  reticulate:::system2t(fs::path_rel("~/.local/bin/uv"), paste(c(...), collapse = " "))
+  reticulate:::system2t(
+    fs::path_rel("~/.local/bin/uv"),
+    paste(c(...), collapse = " ")
+    )
 }
 
-uv("venv")
+uv("venv", "--color", "never")
 use_virtualenv(here::here(".venv"))
 
 uv_install <- function(..., python = py_exe()) {
-  uv("pip install",
+  uv(
+    "pip install",
      "--python", shQuote(fs::path_rel(python)),
-     c(...))
+     c("--color", "never", ...)
+    )
 }
 
-uv_install("polars")
-
-uv_install("--no-progress", "pandas")
-
-uv_install("--color", "never", "jax")
-
-uv_install("--color", "never", "polars")
+uv_install("polars", "jax")
+uv_install("jax")
 
 
 #fs::dir_delete(here::here(".venv"))
