a9527c23b8270a9050a7766a22a2be87dac61fbc
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-04-20 15:47:00 -0500

    Switches to linear model, tests out pin

--- endtoend/demo.R
+++ endtoend/demo.R
@@ -1,15 +1,8 @@
 library(tidyverse)
 library(tidymodels)
+library(pins)
 
 lendingclub_dat <- read_csv("endtoend/loans_full_schema.csv")
-
-colnames(lendingclub_dat)
-
-lendingclub_dat |>
-  mutate(across(c(starts_with("annual")), ~ as.numeric(.))) |> 
-  select(interest_rate, starts_with("annual")) |> 
-  head()
-
 
 set.seed(1234)
 
@@ -17,7 +10,7 @@ train_test_split <- initial_split(lendingclub_dat)
 lend_train <- training(train_test_split)
 lend_test <- testing(train_test_split)
 
-rec_obj <- recipe(interest_rate ~ ., data = lend_train) |> 
+red_rec_obj <- recipe(interest_rate ~ ., data = lend_train) |>
   step_mutate(homeownership = factor(homeownership, levels = c("MORTGAGE", "RENT", "OWN"))) |> 
   step_rm(emp_title, state, state, application_type, verified_income, verification_income_joint, loan_purpose, application_type, grade, sub_grade, issue_month, loan_status, initial_listing_status, disbursement_method) |> 
   step_zv(all_predictors()) |>   
@@ -25,39 +18,31 @@ rec_obj <- recipe(interest_rate ~ ., data = lend_train) |>
   step_normalize(all_numeric_predictors()) |>
   step_impute_mean(all_numeric_predictors())
 
-rec_obj
+lend_linear <- 
+  linear_reg()
 
-lend_lasso <- 
-  linear_reg(penalty = tune(), mixture = 1) |> 
-  set_engine("glmnet")
-
-lend_lasso_wflow <-
+lend_linear_wflow <-
   workflow() |>
-  add_model(lend_lasso) |>
-  add_recipe(rec_obj)
+  add_model(lend_linear) |>
+  add_recipe(red_rec_obj)
 
-
-lambda_grid <- 
-  grid_regular(penalty(), levels = 50)
-
-lasso_grid <- 
-  lend_lasso_wflow |> 
-  tune_grid(grid = lambda_grid, resamples = vfold_cv(lend_train))
-
-lasso_grid
-
-lasso_grid |> 
-  collect_metrics()
-
-lasso_grid |> 
-  autoplot()
-
-final_lasso_wflow <- 
-  lend_lasso_wflow |> 
-  finalize_workflow(list(penalty = 0.1))
-
-lend_lasso_fit <-
-  final_lasso_wflow |>
+lend_linear_fit <-
+  lend_linear_wflow |>
   fit(data = lend_train)
 
-lend_lasso_fit
+
+board <- board_databricks("/Volumes/sol_eng_demo_nickp/end-to-end/r-models")
+pin_write(board, lend_linear_fit, "lending-model-linear")
+
+
+library(tidyverse)
+library(tidymodels)
+library(pins)
+board <- board_databricks("/Volumes/sol_eng_demo_nickp/end-to-end/r-models")
+model <- pin_read(board, "lending-model-linear")
+lendingclub_dat <- read_csv("endtoend/loans_full_schema.csv")
+predict(model, lendingclub_dat)
+
+
+
+
