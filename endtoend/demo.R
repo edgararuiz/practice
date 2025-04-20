47ea4fe438a4f752968788ebd052f81da79c4ecc
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2025-04-20 16:21:19 -0500

    Switches to using table

--- endtoend/demo.R
+++ endtoend/demo.R
@@ -44,5 +44,47 @@ lendingclub_dat <- read_csv("endtoend/loans_full_schema.csv")
 predict(model, lendingclub_dat)
 
 
+# ----------------------------- data from table -------------------------------
+library(tidymodels)
+library(sparklyr)
+library(dplyr)
+
+sc <- spark_connect(method = "databricks_connect", version = "16.3")
+
+tbl_lending <- tbl(sc, I("sol_eng_demo_nickp.`end-to-end`.loans_full_schema"))
+
+local_lending <- tbl_lending |> 
+  sample_n(1000) |> 
+  collect()
 
 
+set.seed(1234)
+
+train_test_split <- initial_split(local_lending)
+lend_train <- training(train_test_split)
+lend_test <- testing(train_test_split)
+
+red_rec_obj <- recipe(interest_rate ~ ., data = lend_train) |>
+  step_mutate(
+    homeownership = factor(homeownership, levels = c("MORTGAGE", "RENT", "OWN")),
+    debt_to_income = as.numeric(debt_to_income),
+    annual_income_joint = as.numeric(annual_income_joint),
+    debt_to_income_joint = as.numeric(debt_to_income_joint),
+    months_since_last_delinq = as.numeric(months_since_last_delinq)
+    ) |> 
+  step_rm(emp_title, state, state, application_type, verified_income, verification_income_joint, loan_purpose, application_type, grade, sub_grade, issue_month, loan_status, initial_listing_status, disbursement_method) |> 
+  step_zv(all_predictors()) |>   
+  step_integer(homeownership) |> 
+  step_normalize(all_numeric_predictors()) |>
+  step_impute_mean(all_numeric_predictors())
+
+lend_linear <- linear_reg()
+
+lend_linear_wflow <- workflow() |>
+  add_model(lend_linear) |>
+  add_recipe(red_rec_obj)
+
+lend_linear_fit <- lend_linear_wflow |>
+  fit(data = lend_train)
+
+lend_linear_fit
